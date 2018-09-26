211384f48229a71d7187789c012b1b57bd791adb
jgauna236 <jgauna236@hotmail.com>
2018-09-26 10:08:20 -0300

    intento de semaforo

--- app.ino
+++ app.ino
@@ -1,9 +1,77 @@
+#define LEDAMARILLO1 2
+#define LEDROJO1 3
+#define LEDAMARILLO2 4
+#define LEDROJO2 5
+
+
+    //Tiempo que se tarda de rojo a verde 
+    long tiempoCambio=2000; 
+
+
+    //booleano para coarroborar que semaforo esta encendido
+    boolean activo = true;
+
+
 void setup()
 {
-	
+
+
+ 
+    // Modo entrada/salida de los pines
+    pinMode(LEDAMARILLO1, OUTPUT);
+    pinMode(LEDROJO1, OUTPUT);
+    pinMode(LEDAMARILLO2, OUTPUT);
+    pinMode(LEDROJO2, OUTPUT);
+//    pinMode(PULSADOR1, INPUT);
+//    pinMode(PULSADOR2, INPUT);
+    
+    // Apagamos todos los LEDs
+    digitalWrite(LEDAMARILLO1, LOW);
+    digitalWrite(LEDROJO1, LOW);
+    digitalWrite(LEDAMARILLO2, LOW);
+    digitalWrite(LEDROJO2, LOW);
+    
+    // Estado inicial: semáforo 1 activo, semáforo 2 no activo
+    digitalWrite(LEDROJO2, HIGH);
+
+
+
+
 }
+
+
+void cambiarSemaforos(){
+    if (activo){
+        digitalWrite(LEDAMARILLO2, HIGH);
+        digitalWrite(LEDROJO2, LOW);
+        delayMicroseconds(tiempoCambio);
+        digitalWrite(LEDAMARILLO2, LOW);
+        delayMicroseconds(tiempoCambio);
+        digitalWrite(LEDROJO1, HIGH);
+
+        activo = false;
+    }else{
+        digitalWrite(LEDAMARILLO1, HIGH);
+        digitalWrite(LEDROJO1, LOW);
+        delayMicroseconds(tiempoCambio);
+        digitalWrite(LEDAMARILLO1, LOW);
+        delayMicroseconds(tiempoCambio);
+        digitalWrite(LEDROJO2, HIGH);
+      
+      	activo = true;
+    }
+
+
+}
+
 
 void loop()
 {
-	
+
+    cambiarSemaforos();
 }
+
+
+
+
+

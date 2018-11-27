78616a19235e53395682ce5b274dab539773092f
jgauna236 <jgauna236@hotmail.com>
2018-11-27 01:21:56 -0300

    funciona completo (creo)

--- app/app.ino
+++ app/app.ino
@@ -10,10 +10,10 @@
 #define OUT_SBG A3
 #define OUT_SBY A4
 #define OUT_SBR A5
-#define OUT_PAB 7
-#define OUT_PAR 8
-#define OUT_PBB 9
-#define OUT_PBR 10
+#define OUT_PAG 8
+#define OUT_PAR 9
+#define OUT_PBG 10
+#define OUT_PBR 11
 
 #define IN_PA 2
 #define IN_PB 3
@@ -23,8 +23,8 @@
 #define IN_B_start  6
 #define IN_B_finish 7
 
-#define T_MIN 45
-#define T_MAX 120
+#define T_MIN 3
+#define T_MAX 10
 
 const int tiempoAntirrebote = 10;
 int count_cars[2]={0,0};
@@ -34,11 +34,11 @@ int timer_flag=0;
 int count_flag=0;
 int PA_flag=0;
 int PB_flag=0;
-int seconds=T_MIN;
-int status=0;
+int seconds=0;
+int estado=0;
 
 void setup(){
-
+  
   //Serial
   Serial.begin(9600);
   while(!Serial);
@@ -54,10 +54,10 @@ void setup(){
   pinMode(OUT_SBY, OUTPUT); //Amarrillo
   pinMode(OUT_SBR, OUTPUT); //Rojo
   //Semáforo Peaton Calle A
-  pinMode(OUT_PAB, OUTPUT); //Azul
+  pinMode(OUT_PAG, OUTPUT); //Azul
   pinMode(OUT_PAR, OUTPUT); //Rojo
   //Semáforo Peaton Calle B
-  pinMode(OUT_PBB, OUTPUT); //Azul
+  pinMode(OUT_PBG, OUTPUT); //Azul
   pinMode(OUT_PBR, OUTPUT); //Rojo
 
   //Entradas
@@ -94,19 +94,19 @@ void loop(){
     seconds++;
   }
   listen_inputs();
-  set_status();
+  set_estado();
 }
 
  boolean antirrebote(int pin)
   {
     int contador = 0;
-    boolean estado;
+    boolean restado;
     boolean estadoAnterior;
    
     do
     {
-      estado = digitalRead(pin);
-      if(estado != estadoAnterior)
+      restado = digitalRead(pin);
+      if(restado != estadoAnterior)
       {
         contador = 0;
         estadoAnterior = estado;
@@ -118,10 +118,10 @@ void loop(){
       delay(1);
     } while(contador < tiempoAntirrebote);
    
-    return estado;
+    return restado;
   }
 void set_leds(){
-  switch (status) {
+  switch (estado) {
     case 0:
       digitalWrite(OUT_SAG, HIGH);
       digitalWrite(OUT_SAY, LOW);
@@ -129,9 +129,9 @@ void set_leds(){
       digitalWrite(OUT_SBG, LOW);
       digitalWrite(OUT_SBY, LOW);
       digitalWrite(OUT_SBR, HIGH);
-      digitalWrite(OUT_PAB, LOW);
+      digitalWrite(OUT_PAG, LOW);
       digitalWrite(OUT_PAR, HIGH);
-      digitalWrite(OUT_PBB, HIGH);
+      digitalWrite(OUT_PBG, HIGH);
       digitalWrite(OUT_PBR, LOW);
     break;
     case 1:
@@ -141,9 +141,9 @@ void set_leds(){
       digitalWrite(OUT_SBG, LOW);
       digitalWrite(OUT_SBY, LOW);
       digitalWrite(OUT_SBR, HIGH);
-      digitalWrite(OUT_PAB, LOW);
+      digitalWrite(OUT_PAG, LOW);
       digitalWrite(OUT_PAR, HIGH);
-      digitalWrite(OUT_PBB, LOW);
+      digitalWrite(OUT_PBG, LOW);
       digitalWrite(OUT_PBR, !digitalRead(OUT_PBR));
     break;
     case 2:
@@ -153,9 +153,9 @@ void set_leds(){
       digitalWrite(OUT_SBG, LOW);
       digitalWrite(OUT_SBY, LOW);
       digitalWrite(OUT_SBR, HIGH);
-      digitalWrite(OUT_PAB, LOW);
+      digitalWrite(OUT_PAG, LOW);
       digitalWrite(OUT_PAR, HIGH);
-      digitalWrite(OUT_PBB, LOW);
+      digitalWrite(OUT_PBG, LOW);
       digitalWrite(OUT_PBR, HIGH);
     break;
     case 3:
@@ -165,9 +165,9 @@ void set_leds(){
       digitalWrite(OUT_SBG, HIGH);
       digitalWrite(OUT_SBY, LOW);
       digitalWrite(OUT_SBR, LOW);
-      digitalWrite(OUT_PAB, HIGH);
+      digitalWrite(OUT_PAG, HIGH);
       digitalWrite(OUT_PAR, LOW);
-      digitalWrite(OUT_PBB, LOW);
+      digitalWrite(OUT_PBG, LOW);
       digitalWrite(OUT_PBR, HIGH);
     break;
     case 4:
@@ -177,9 +177,9 @@ void set_leds(){
       digitalWrite(OUT_SBG, LOW);
       digitalWrite(OUT_SBY, HIGH);
       digitalWrite(OUT_SBR, LOW);
-      digitalWrite(OUT_PAB, LOW);
+      digitalWrite(OUT_PAG, LOW);
       digitalWrite(OUT_PAR, !digitalRead(OUT_PAR));
-      digitalWrite(OUT_PBB, LOW);
+      digitalWrite(OUT_PBG, LOW);
       digitalWrite(OUT_PBR, HIGH);
     break;
     case 5:
@@ -189,67 +189,57 @@ void set_leds(){
       digitalWrite(OUT_SBG, LOW);
       digitalWrite(OUT_SBY, LOW);
       digitalWrite(OUT_SBR, HIGH);
-      digitalWrite(OUT_PAB, LOW);
+      digitalWrite(OUT_PAG, LOW);
       digitalWrite(OUT_PAR, HIGH);
-      digitalWrite(OUT_PBB, LOW);
+      digitalWrite(OUT_PBG, LOW);
       digitalWrite(OUT_PBR, HIGH);
     break;
   }
 }
 
-void set_status(){
-  if(seconds < T_MIN){
-    switch (status) {
-      case 1: case 4:
-        if(seconds > 3){
+void set_estado(){
+  if(seconds > T_MIN){
+    switch (estado) {
+      case 0:
+        if(((count_cars[SB] - count_cars[SA])>2)|| PA_flag == 1){
+          estado++;
+          PA_flag = 0;
           seconds = 0;
-          status++;
         }
       break;
+      case 3:
+        if(((count_cars[SA] - count_cars[SB])>2) || PB_flag == 1){
+          estado ++;
+          PB_flag = 0;
+          seconds = 0;
+        }
+        break;
+      case 1:
+        estado++;
+        seconds=0;
+        break;
       case 2:
-        seconds = 0;
-        status = 3;
-      break;
+        estado++;
+        seconds=0;
+        break;
+      case 4:
+        estado++;
+        seconds=0;
+        break;
       case 5:
-        seconds = 0;
-        status = 0;
-      break;
-    }
-  } else if(seconds > T_MAX) {
-    switch (status) {
-      case 0:
-      	if(count_cars[SB] > 0 || PA_flag == 1){
-      	  status = 1;
-          PA_flag = 0;
-          seconds = 0;
-        }
-      break;
-      case 3:
-      	if(count_cars[SA] > 0 || PB_flag == 1){
-      	  status = 4;
-          PB_flag = 0;
-          seconds = 0;
-        }
-      break;
-    }
-  } else {
-    switch (status) {
-      case 0:
-        if(count_cars[SB] > count_cars[SA] || PA_flag == 1){
-      	  status = 1;
-          PA_flag = 0;
-          seconds = 0;
-        }
-      break;
-      case 3:
-      	if(count_cars[SA] > count_cars[SA] || PB_flag == 1){
-      	  status = 4;
-          PB_flag = 0;
-          seconds = 0;
-        }
-      break;
+        estado=0;
+        seconds=0;
+        break;
     }
   }
+  if(seconds > T_MAX) {
+    seconds = 0;
+    if (estado != 5){
+      estado++;
+    }else {
+      estado=0;
+    }
+  } 
 }
 
 void listen_inputs(){
@@ -271,39 +261,43 @@ void listen_inputs(){
   if(inputs_prev[0] > inputs_now[0]){
     if(antirrebote(IN_A_start)){
       count_cars[SA]++;
-      Serial.println("Entro un vehiculo en calle A");
+      Serial.print("Vehículos en calle A: ");
+      Serial.println(count_cars[SA]);
     }
   }
   if(inputs_prev[1] > inputs_now[1]){
     if(antirrebote(IN_A_finish)){
       count_cars[SA]--;
-      Serial.println("Salió un vehiculo en calle A");
-      if(count_cars[SA] > 0){count_cars[SA] = 0;}
+      if(count_cars[SA] < 0){count_cars[SA] = 0;}
+      Serial.print("Vehículos en calle A: ");
+      Serial.println(count_cars[SA]);
      }
   }
   if(inputs_prev[2] > inputs_now[2]){
     if(antirrebote(IN_B_start)){
       count_cars[SB]++;
-      Serial.println("Entro un vehiculo en calle B");
+      Serial.print("Vehículos en calle B: ");
+      Serial.println(count_cars[SB]);
     } 
   }
   if(inputs_prev[3] > inputs_now[3]){
     if(antirrebote(IN_B_finish)){
       count_cars[SB]--;
-      Serial.println("Salió un vehiculo en calle B");
       if(count_cars[SB] < 0){count_cars[SB] = 0;}
+      Serial.print("Vehículos en calle B: ");
+      Serial.println(count_cars[SB]);
    }  
   }
   if(inputs_prev[4] != inputs_now[4]){
     if(antirrebote(IN_PA)){
       Serial.println("Peaton presiono boton en en calle A");
-      if(status != 3 && status != 4){PA_flag = 1;}
+      if(estado != 3 && estado != 4){PA_flag = 1;}
    }  
   } 
   if(inputs_prev[5] != inputs_now[5]){
     if(antirrebote(IN_PB)){
       Serial.println("Peaton presiono boton en en calle B");
-      if(status != 0 && status != 1){PB_flag = 1;}
+      if(estado != 0 && estado != 1){PB_flag = 1;}
     }  
   }
 }
